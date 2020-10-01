acb9fee909ea7b579d021a8dfa89ed6dabec4be3
Patryk Kurzeja <patryk.kurzeja@monterail.com>
2020-10-01 22:40:07 +0200

    Add computed property

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
-    <p>Capacity: {{ capacity }}</p>
+    <p>Spaces left {{ spacesLeft }} out of apacity: {{ capacity }}</p>
     <button @click="increaseCapacity()">Increase capacity</button>
+
+    <h2>ATTENDING</h2>
+    <ul>
+      <li v-for="(name, i) in attending" :key="i">
+        {{ name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from "vue";
 export default {
   setup() {
-    const capacity = ref(3)
+    const capacity = ref(3);
+    const attending = ref(["Tim", "Bob", "John"]);
+
+    const spacesLeft = computed(() => {
+      return capacity.value - attending.value.length;
+    });
 
     function increaseCapacity() {
-      capacity.value++
+      capacity.value++;
     }
 
-    return { capacity, increaseCapacity };
+    return { capacity, attending, increaseCapacity, spacesLeft };
   },
 };
 </script>
