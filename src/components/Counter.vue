6bd5f77e97f48fc5157f4fab0fcebb319a9862b3
Patryk Kurzeja <patryk.kurzeja@monterail.com>
2020-10-01 22:31:00 +0200

    Add initial setup for compposition appi

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Capacity: {{ capacity }}</p>
+    <button @click="increaseCapacity()">Increase capacity</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const capacity = ref(3)
+
+    function increaseCapacity() {
+      capacity.value++
+    }
+
+    return { capacity, increaseCapacity };
+  },
+};
+</script>
+
+<style lang="css" scoped></style>
